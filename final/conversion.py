41b9f340aac5a4ae8186eb7d10fc8bcda003da5e
karl <karl.sondeji@etu.univ-tours.fr>
2023-04-13 23:28:34 +0200

    Amélioration dde la conversion des données en graphe

--- final/conversion.py
+++ final/conversion.py
@@ -4,23 +4,27 @@ Fonctionnalités de conversion du problème brute vers un graphe.
 """
 
 from .data import Donnees
-from serde.json import to_json, from_json
+from enum import Enum
+from itertools import product
 
-essai = Donnees(
-    nutriments=["Kcal", "Protéines", "Glucides",
-                "Lipides", "Fer", "Calcium", "Fibre"],
-    aliments_index=25,
-    contraintes_nutriments=[(0, 5)],
-    contraintes_couts=[12],
-    besoins_journaliers=[
-        ("Kcal", 2000),
-        ("Protéines", 75),
-        ("Glucides", 225),
-        ("Lipides", 90),
-        ("Fer", 9),
-        ("Fibre", 45),
-    ],
-)
+import networkx as nx
 
-code = to_json(essai)
-code
+def problem_to_graph(dp):
+    # Création du graphe
+    G = nx.DiGraph()
+
+    # Ajout des nœuds ingrédients
+    for i, ingredient_name in enumerate(dp.get_ingredient_names()):
+        G.add_node(ingredient_name, bipartite=0, nutrient_contribution=dp.coeff_df.iloc[:, i].tolist())
+        
+    # Ajout des nœuds nutriments
+    for j, nutrient_name in enumerate(dp.get_nutrient_names()):
+        G.add_node(nutrient_name, bipartite=1, nutrient_requirement=dp.Marie_df.loc[nutrient_name, "Marie"])
+    
+    # Ajout des arrêtes entre ingrédients et nutriments
+    for i in range(dp.coeff.shape[1]):
+        for j in range(dp.coeff.shape[0] - 1):
+            if dp.coeff[j, i] != 0:
+                G.add_edge(dp.get_ingredient_names()[i], dp.get_nutrient_names()[j], weight=dp.coeff[j, i])
+
+    return G
